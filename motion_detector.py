6cfde817340f5718b1ef80a9c68f10a7c789b0ea
Bipeen Acharya <acharyabipeen@gmail.com>
2019-06-27 15:51:45 -0700

    Basic motion detector working

--- motion_detector.py
+++ motion_detector.py
@@ -27,7 +27,7 @@ firstFrame = None
 # loop over the frames of the video
 while True:
     #grab the current frame and initialize the occupied/unoccupied text
-    frame = vs.Read()
+    frame = vs.read()
     frame = frame if args.get("video", None) is None else frame[1]
     text = "Unoccupied"
 
@@ -53,7 +53,7 @@ while True:
 
     # dilate the threshold image to fill in holes, then find contours
     # on thresholded image
-    thresh = cv2.dilate(thres, None, iterations=2)
+    thresh = cv2.dilate(thresh, None, iterations=2)
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
@@ -69,3 +69,25 @@ while True:
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
         text = "Occupied"
+
+        # draw the text and timestamp on the frame
+        cv2.putText(frame, "Room Status: {}".format(text), (10,20), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), 
+                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,0,255), 1)
+
+        # show the frame and record if the user presses a key
+        cv2.imshow("Security Feed", frame)
+        cv2.imshow("Thresh", thresh)
+        cv2.imshow("Frame Delta", frameDelta)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key is pressed, break from the loop
+        if key == ord("q"):
+            break
+
+# cleanup the camera and close any open windows
+vs.stop() if args.get("video", None) is None else vs.release()
+cv2.destroyAllWindows()
+
+
